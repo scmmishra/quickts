bbd3c8cb10dd12768c82256564d8a899aaa6eab6
Shivam Mishra <scm.mymail@gmail.com>
2021-07-31 18:47:02 +0530

    fix: installation directory

--- src/commands/create.ts
+++ src/commands/create.ts
@@ -85,6 +85,7 @@ export default async function create(opts: ProjectOpts) {
     const installSpinner = ora(installing(deps.sort())).start()
 
     try {
+      process.chdir(projectPath)
       await installPackages(deps)
       installSpinner.succeed('Installed dependencies')
     } catch (error) {
