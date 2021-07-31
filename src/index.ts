8900f50b7dbbb6e5e97d61818e55a39da5070ecb
Shivam Mishra <scm.mymail@gmail.com>
2021-07-31 20:16:41 +0530

    chore: remove import

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ import { ensureSafeProjectPath } from './utils/files'
 import { licenses, optionalFeatures } from './constants/choices'
 
 // Terminal
-import { bold, dim, yellow, green } from 'kolorist'
+import { bold, dim, yellow } from 'kolorist'
 import { prompt, promptMultiSelect, promptSelect } from './utils/prompts'
 import fullname from 'fullname'
 import create from './commands/create'
