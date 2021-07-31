01c559311b2aa8bc2606b2a5cf8c50bbf0d1af98
Shivam Mishra <scm.mymail@gmail.com>
2021-07-31 13:07:53 +0530

    feat: kicstart with sade

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
+#!/usr/bin/env node
+import sade from "sade";
+import { version } from "../package.json";
 
+const quickts = sade("quickts");
+
+quickts.version(version);
+
+quickts.command("hello").action(() => {
+  console.log("helloworld");
+});
+
+quickts.parse(process.argv);
