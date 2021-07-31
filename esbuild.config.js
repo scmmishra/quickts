e0d41db0c4fc01ae5429d8d714458e9922760abe
Shivam Mishra <scm.mymail@gmail.com>
2021-07-31 12:39:48 +0530

    feat: better logging for esbuild

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,8 +1,15 @@
 const esbuild = require("esbuild");
-const { red, cyan } = require("kolorist");
+const { red, cyan, green, dim } = require("kolorist");
 
 // Automatically exclude all node_modules from the bundled version
 const { nodeExternalsPlugin } = require("esbuild-node-externals");
+
+function logChange(metafile) {
+  console.log(metafile);
+  console.log("Detected change in files: ", green(Object.keys(metafile.inputs).join(", ")));
+  console.log("Generated output: ", green(Object.keys(metafile.outputs).join(", ")));
+  console.log("");
+}
 
 esbuild
   .build({
@@ -10,11 +17,14 @@ esbuild
     outfile: "dist/index.js",
     bundle: true,
     minify: false,
+    metafile: true,
     watch: {
       onRebuild(error, result) {
-        if (error) console.error("watch build failed:", error);
-        else {
-          console.log("Rebuilding...\n");
+        if (error) {
+          console.error("There was an error rebuilding changes.", error);
+          console.log(dim(error.stack));
+        } else {
+          logChange(result.metafile);
           if (result.errors.length) {
             console.log(red("Erros: "), result.errors);
           }
